42766db4bf1f6a9962599026b32b001559b62815
Jose Suarez <jolusuvi@gmail.com>
2023-09-22 11:07:13 -0500

    Fix asignment

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -12,21 +12,21 @@ if __name__ == '__main__':
     param = argv[1]
     user = requests.get(url + "users?id={}".format(param))
     # Transforms JSON data in Python objects
-    user.json()
-    name = user[0]['name']
+    user = user.json()
+    name = user[0]["name"]
     todos = requests.get(url + "todos?userId={}".format(param))
     # Transforms JSON data in Python objects
-    todos.json()
+    todos = todos.json()
     done = requests.get(url + "todos?userId={}&completed=true".format(param))
     # Transforms JSON data in Python objects
-    done.json()
+    done = done.json()
     done_list = []
 
     print("Employee {} is done with tasks({}/{}):"
-          .format(name, len(done), todos))
+          .format(name, len(done), len(todos)))
 
     # n is a dictionary and title is the key
-    for n in todos:
+    for n in done:
         done_list.append("\t {}".format(n["title"]))
     for task in done_list:
         print(task)
